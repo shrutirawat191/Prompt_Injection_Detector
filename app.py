076c5a3c1a9fefd25a36ec437e3a648188fb711a
shruti <raw503@users.noreply.huggingface.co>
2026-06-09 12:14:33 +0000

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import os
 from transformers import pipeline
 
 # Load model (same as before)
-MODEL_DIR = "/kaggle/working/training_output/prompt_injection_model"
+MODEL_DIR = "/Prompt_Injection_Detector/model"
 DEFAULT_THRESHOLD = 0.75
 
 classifier = pipeline("text-classification", model=MODEL_DIR, tokenizer=MODEL_DIR, device=0)
